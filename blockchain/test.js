dd7c4786cecc85fbd3009783b498ea1305129fa5
Elif Turan <56327195+elifyagmurturan@users.noreply.github.com>
2021-04-14 23:56:16 +0300

    Started blockchain JS  project

---
+++ blockchain/test.js
@@ -0,0 +1,23 @@
+const Blockchain = require('./blockchain')
+
+const bitcoin = new Blockchain();
+// // console.log(bitcoin);
+// bitcoin.createNewBlock(3872, 'ASDFGHJKLQWERTYUI', '8432928xsd49482282f');
+// // console.log(bitcoin);
+// bitcoin.createNewBlock(3872, 'ASDFGHJKLQWERTYUI', '8432928xsd49482282f');
+// // console.log(bitcoin);
+// bitcoin.createNewTransaction(200, 'HELLOWORLD123', 'HASH1234567');
+// // console.log(bitcoin);
+// bitcoin.createNewBlock(25932, 'HELLOWORLD12345', 'HASH123456789');
+// console.log(bitcoin);
+const previousBlockHash = '5049525839538EEABC238C1D27C35692E11';
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: '5893298EABCD71',
+        recipient: '3EDC45677E9',  
+    }  
+]
+const nonce = 50;
+bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce );
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
