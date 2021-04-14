d9b9a5bf3a4e3fecfbb7188628af23ea8ade1ed1
Elif Turan <56327195+elifyagmurturan@users.noreply.github.com>
2021-04-15 00:09:22 +0300

    added fn to check hash validity

--- blockchain/test.js
+++ blockchain/test.js
@@ -2,14 +2,19 @@ const Blockchain = require('./blockchain')
 
 const bitcoin = new Blockchain();
 // // console.log(bitcoin);
+
 // bitcoin.createNewBlock(3872, 'ASDFGHJKLQWERTYUI', '8432928xsd49482282f');
 // // console.log(bitcoin);
+
 // bitcoin.createNewBlock(3872, 'ASDFGHJKLQWERTYUI', '8432928xsd49482282f');
 // // console.log(bitcoin);
+
 // bitcoin.createNewTransaction(200, 'HELLOWORLD123', 'HASH1234567');
 // // console.log(bitcoin);
+
 // bitcoin.createNewBlock(25932, 'HELLOWORLD12345', 'HASH123456789');
 // console.log(bitcoin);
+
 const previousBlockHash = '5049525839538EEABC238C1D27C35692E11';
 const currentBlockData = [
     {
@@ -20,4 +25,6 @@ const currentBlockData = [
 ]
 const nonce = 50;
 bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce );
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
+
+console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
